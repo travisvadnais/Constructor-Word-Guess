d1e491e1b18c3a30ff3bfb07e47546041b3a78ed
Travis Vadnais <travisvadnais@gmail.com>
2018-04-27 19:49:20 -0400

    add validator info and clean up code

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var Word = require("./word.js");
 var inquire = require('inquirer');
 var fs = require('fs');
 var colors = require("colors");
+var validator = require('validator');
 
 //Set up array of words or phrases
 var hangmanWords = ["After the Rain", "Animals", "Another Hole in the Head", "Because of You", "Believe it or Not", "Bottoms Up", "Breathe", "Burn it to the Ground", "Coin for the Ferryman", "Cowboy Hat", "Curb", "D C", "Deep", "Detangler", "Diggin This", "Dirty Laundry", "Do This Anymore", "Dont Ever Let It End", "Edge of a Revolution", "Every Time Were Together", "Everything I Wanna Do", "Falls Back On", "Far Away", "Feed the Machine", "Feelin Way Too Damn Good", "Fight for All the Wrong Reasons", "Figured You Out", "Flat on the Floor", "Fly", "Follow You Home", "For the River", "Get Em Up", "Good Times Gone", "Got Me Runnin Round", "Gotta Be Somebody", "Gotta Get Me Some", "Hangnail", "Hold Out Your Hand", "Holding on to Heaven", "Hollywood", "Home", "How You Remind Me", "I Dont Have", "Id Come for You", "If Everyone Cared", "If Today Was Your Last Day", "In Front of Me", "Just Four", "Just to Get High", "Kiss it Goodbye", "Leader of Men", "Learn the Hard Way", "Left", "Legs", "Little Friend", "Lullaby", "Make Me Believe Again", "Midnight Queen", "Million Miles an Hour", "Miss You", "Money Bought", "Must Be Nice", "Never Again", "Never Gonna Be Alone", "Next Contestant", "Next Go Round", "Not Leavin Yet", "Old Enough", "One Last Run", "Photograph", "Pusher", "Rockstar", "S E X", "Satellite", "Saturday Nights Alright For Fighting", "Savin Me", "Sea Groove", "See You at the Show", "Shakin Hands", "She Keeps Me Up", "Shouldve Listened", "Side of a Bullet", "Silent Majority", "Sister Sin", "Slow Motion", "Someday", "Someone That Youre With", "Something in Your Mouth", "Song on Fire", "The Betrayal Act I", "The Betrayal Act III", "The Hammers Comin Down", "This Afternoon", "This Means War", "Throw Yourself Away", "Too Bad", "Truck", "Trying Not to Love You", "We Will Rock You", "What Are You Waiting For", "When We Stand Together", "Where Do I Hide", "Where", "Window Shopper", "Woke Up This Morning", "Worthy to Say", "Yanking Out My Heart"];
@@ -51,7 +52,7 @@ function guessLetter() {
     //Run a check to see if they lost
     if (guessesRemaining === 0) {
         //If so, let them know they're screwed and what their word was
-        console.log("You lose!  Your song was:");
+        console.log("You lose!  Do you even like Nickelback?  Your song was:");
         console.log("\n              " + gameWord.word + "!\n");
         //Run a check to see if they want to play again
         playAgainCheck();
@@ -91,11 +92,16 @@ function checkGuess(letter) {
     if ((gameWord.currGameStr(gameWord).indexOf(guess) != -1) || (guessedLetters.indexOf(guess) != -1)) {
         console.log("\nResult: You already guessed " + guess + ". Try again");
     }
-    
-    //First condition checks the guessedLetters array for the guessed letter
-    //Second condition checks the gameword to see if that letter exists
-    //Both conditions must be met to push a letter to the array b/c we don't want duplicates, and we don't want a letter from the gameword to be pushed to the guessed letters array
+    //Check to make sure only one letter was entered
+    else if (guess.length > 1) {
+        console.log("\nYou can only pick ONE letter, haven't you ever played this game before?  Try again.")
+    }
+    else if (!validator.isAlpha(guess)) {
+        console.log("\nThat's not a letter.  Are you serious with this?  Pick a letter");
+    }
+    //Check to make sure the letter exists in the word and the game is still going
     else if ((guessedLetters.indexOf(guess) == -1) && (currWord.indexOf(guess) == -1) && (guessesRemaining > 0)) {
+        //Push the letter into the guessed letters array if it was wrong and decrement the guesses remaining
         guessedLetters.push(guess);
         guessesRemaining--;
         console.log("Result: Wrong!");
